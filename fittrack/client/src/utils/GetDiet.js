158b28ebda350d6a0446553ddfff200d3e316ee3
Mike Prap <michaelpraplaski@yahoo.com>
2019-11-05 15:18:10 -0500

    got the api to work

--- fittrack/client/src/utils/GetDiet.js
+++ fittrack/client/src/utils/GetDiet.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 // Export an object containing methods we'll use for accessing the Dog.Ceo API
 
-export default {
-  getDiet: function() {
+const GetDiet = () => {
     return axios.get("https://api.edamam.com/search?q=chicken&app_id=$8461c4d2&app_key=$9e1260eb6b88a84e3b538584b38b0ecc&from=0&to=3&calories=591-600&diet=low-carb&high-protein");
-  },
+  
  
 };
+
+export default GetDiet
