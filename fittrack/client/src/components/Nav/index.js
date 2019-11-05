c5740e5bc7237d2a046d880d7be4f88a4b931caa
Joshua <fowlerjosh93@gmail.com>
2019-11-04 20:06:22 -0500

    Added navabar, changed form to include parameters needed, switched css framework from bootstrap to Bulma

--- fittrack/client/src/components/Nav/index.js
+++ fittrack/client/src/components/Nav/index.js
@@ -33,52 +33,25 @@ class Nav extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-2">
-        <Link className="navbar-brand" to="/">
-          Auth Plate
-        </Link>
-        <button
-          onClick={this.toggleNav}
-          className="navbar-toggler"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className={`${this.state.open ? "" : "collapse "}navbar-collapse`} id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link
-                onClick={this.toggleNav}
-                className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}
-                to="/login"
-              >
-                Login
-              </Link>
-            </li>
-            {/* <li className="nav-item">
-              <Link
-                onClick={this.toggleNav}
-                className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}
-                to="/"
-              >
-                Search
-              </Link>
-            </li> */}
-            {/* <li className="nav-item">
-              <Link
-                onClick={this.toggleNav}
-                className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}
-                to="/saved"
-              >
-                Saved
-              </Link>
-            </li> */}
-          </ul>
+<nav className="navbar is-dark" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io">
+            <h1>Fit-Track</h1>
+            {/* <img src="https://bulma.io/images/bulma-logo.png" width={112} height={28} /> */}
+          </a>
         </div>
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                <a className="button is-primary">
+                  <strong>Sign up</strong>
+                </a>
+                <a className="button is-light">
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
       </nav>
     );
   }
--- fittrack/client/src/components/RegisterForm/index.js
+++ fittrack/client/src/components/RegisterForm/index.js
@@ -6,31 +6,71 @@ class RegisterForm extends React.Component {
 	// refs
 	form: null;
 	usernameElem: null;
+	ageElem: null;
+	weightElem: null;
+	heightElem: null;
 	passwordElem: null;
 
 	render() {
 		const { onRegister } = this.props;
 		console.log(`Register form props are: ${JSON.stringify(this.props)}`);
-		
+
 		return (
-			
-				<form
-					ref={(elem) => this.form = elem}
-					onSubmit={(e) => {
-						e.preventDefault();
-						return onRegister({
-							username: this.usernameElem.value,
-							password: this.passwordElem.value
-						});
-					}}
-				>
-					<div className="form-group">
-						<input className="form-control" ref={(input) => this.usernameElem = input} type='text' name="username" placeholder='Enter Username' /><br/>
-						<input className="form-control"  ref={(input) => this.passwordElem = input} type='password' name="password" placeholder='Password' /><br/>
-						<button className="btn btn btn-primary" type='submit'>Submit</button>
+
+			<form
+				ref={(elem) => this.form = elem}
+				onSubmit={(e) => {
+					e.preventDefault();
+					return onRegister({
+						username: this.usernameElem.value,
+						password: this.passwordElem.value,
+						age: this.ageElem.value,
+						weight: this.weightElem.value,
+						height: this.heightElem.value
+					});
+				}}
+			>
+				<div className="field">
+					<label className="label">Username</label>
+					<div className="control">
+						<input className="input" ref={(input) => this.usernameElem = input} type='text' name="username" placeholder='Enter Username'/>
 					</div>
-				</form>
-			
+				</div>
+				<div className="field">
+					<label className="label">Age</label>
+					<div className="control">
+						<input className="input" ref={(input) => this.ageElem = input} type='number' name="age" placeholder='Enter Your Age' />
+					</div>
+				</div>
+				<div className="field">
+					<label className="label">Weight</label>
+					<div className="control">
+						<input className="input" ref={(input) => this.weightElem = input} type='number' name="weight" placeholder='Enter Your Weight'/>
+					</div>
+				</div>
+				<div className="field">
+					<label className="label">Height</label>
+					<div className="control">
+						<input className="input" ref={(input) => this.heightElem = input} type='number' name="height" placeholder='Enter Your Height'/>
+					</div>
+				</div>
+				<div className="field">
+					<label className="label">Password</label>
+					<div className="control">
+						<input className="input" ref={(input) => this.passwordElem = input} type='password' name="password" placeholder='Password'/>
+					</div>
+				</div>
+				<button className="button is-rounded is-dark" type='submit'>Submit</button>
+				{/* <div className="form-group">
+					<input className="form-control" ref={(input) => this.usernameElem = input} type='text' name="username" placeholder='Enter Username' /><br />
+					<input className="form-control" ref={(input) => this.ageElem = input} type='number' name="age" placeholder='Enter Your Age' /><br />
+					<input className="form-control" ref={(input) => this.weightElem = input} type='number' name="weight" placeholder='Enter Your Weight' /><br />
+					<input className="form-control" ref={(input) => this.heightElem = input} type='number' name="height" placeholder='Enter Your Height' /><br />
+					<input className="form-control" ref={(input) => this.passwordElem = input} type='password' name="password" placeholder='Password' /><br />
+					<button className="btn btn btn-primary" type='submit'>Submit</button>
+				</div> */}
+			</form>
+
 		)
 	}
 }
