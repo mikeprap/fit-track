8359fa11136395eebc4ff7d4a6317ad69015a9f6
Mike Prap <michaelpraplaski@yahoo.com>
2019-11-06 13:13:14 -0500

    lunch

--- fittrack/client/src/pages/Lunch/index.js
+++ fittrack/client/src/pages/Lunch/index.js
@@ -34,20 +34,7 @@ class ProtectedRoute extends React.Component {
 		this.loadDinner();
 		this.loadVideo();
 	}
-	loadDinner = () => {
-		fetch("https://api.edamam.com/search?q=tilapia&app_id=$8461c4d2&app_key=$9e1260eb6b88a84e3b538584b38b0ecc&from=0&to=5&calories=600&diet=low-fat&mealType=Dinner&diet=low-carb&yield=1")
-			.then(res => {
-				return res.json()
-			}).then(data => {
-				this.setState({
-					image2: data.hits[3].recipe.image,
-					label2: data.hits[3].recipe.label,
-					ingredients2: data.hits[3].recipe.ingredientLines,
-
-				})
-			})
-			.catch(err => console.log(err));
-	};
+	
 	loadLunch = () => {
 		fetch("https://api.edamam.com/search?q=chicken&app_id=$8461c4d2&app_key=$9e1260eb6b88a84e3b538584b38b0ecc&from=0&to=10&calories=600&diet=low-carb&diet=high-protein&yield=1")
 			.then(res => {
@@ -62,33 +49,7 @@ class ProtectedRoute extends React.Component {
 			})
 			.catch(err => console.log(err));
 	};
-	loadBreakfast = () => {
-		fetch("https://api.edamam.com/search?q=eggs&app_id=$8461c4d2&app_key=$9e1260eb6b88a84e3b538584b38b0ecc&from=0&to=3&calories=600&diet=low-fat&mealType=Breakfast&diet=low-carb")
-			.then(res => {
-				return res.json()
-			}).then(data => {
-				this.setState({
-					image1: data.hits[1].recipe.image,
-					label1: data.hits[1].recipe.label,
-					ingredients1: data.hits[1].recipe.ingredientLines,
-
-				})
-			})
-			.catch(err => console.log(err));
-	};
-	loadVideo = () => {
-		fetch("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=absworkout&key=AIzaSyCbe0-7OsepLY88Fig1jKT6pfZuXiL2FAo")
-			.then(res => {
-				return res.json()
-			})
-			.then(res =>
-				this.setState({
-					video: res.items[2].id.videoId
-				})
-			)
-
-
-	}
+	
 
 
 	render() {
@@ -128,11 +89,7 @@ class ProtectedRoute extends React.Component {
 						</Col>
 					</Row>
 
-					{/* <YouTube
-						videoId={this.state.video}
-						opts={opts}
-						onReady={this._onReady}
-					/> */}
+					
 				</div>
 
 
