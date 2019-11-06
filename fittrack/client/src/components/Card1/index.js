d454e2998c7127f0109af826d36ef74c6dac915a
Mike Prap <michaelpraplaski@yahoo.com>
2019-11-06 14:24:08 -0500

    last card

--- fittrack/client/src/components/Card1/index.js
+++ fittrack/client/src/components/Card1/index.js
@@ -4,10 +4,10 @@ import "./style.css";
 function Card1(props) {
     return (
         <div className="card">
-            <img src= {props.image2}></img>
+            <img src= {props.image1}></img>
           <div className="card-body">
-          <h5 className="card-title">{props.label2}</h5>
-          <p className="card-text">{props.ingredients2}</p>
+          <h5 className="card-title">{props.label1}</h5>
+          <p className="card-text">{props.ingredients1}</p>
          </div>
          
         </div>
