c7101d6b6550cd95c0b0c318f77a369baeef5fdb
Mike Prap <michaelpraplaski@yahoo.com>
2019-11-06 13:05:07 -0500

    fixed breakfast page

---
+++ fittrack/client/src/components/BreakfastCard1/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function BreakfastCard1(props) {
+    return (
+        <div className="card">
+            <img src= {props.image}></img>
+          <div className="card-body">
+          <h5 className="card-title">{props.label}</h5>
+          <p className="card-text">{props.ingredients}</p>
+         </div>
+         
+        </div>
+        );
+}
+
+export default BreakfastCard1;
